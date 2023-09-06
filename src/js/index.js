a3829d777da705f059014e0e1ee1e41a5d94d77b
VNagorniy <vnagorniyrnd@gmail.com>
2023-09-06 23:19:15 +0300

    добавление календаря через библиотеку easepick

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
 // import autoCompleteFunc from './modules/autoComplete';
-
 // autoCompleteFunc();
+
+import { easepick } from '@easepick/core';
+import { TimePlugin } from '@easepick/time-plugin';
+
+const picker = new easepick.create({
+  element: document.getElementById('datePicker'),
+  css: ['https://cdn.jsdelivr.net/npm/@easepick/core@1.2.1/dist/index.css', 'https://cdn.jsdelivr.net/npm/@easepick/time-plugin@1.2.1/dist/index.css'],
+  format: 'HH:mm, DD/MM/YY',
+  plugins: [TimePlugin],
+});
 
 // import mobileNav from './modules/mobile-nav.js';
 
