84d4b4bcb280db84754c76f5dda933a686b5abf7
VNagorniy <vnagorniyrnd@gmail.com>
2023-09-06 22:00:35 +0300

    Верстка блока scroll-down

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,7 @@
-import autoComplete from '@tarekraafat/autocomplete.js';
-import countryList from './helpers/countryList';
+// import autoCompleteFunc from './modules/autoComplete';
 
-const autoCompleteJS = new autoComplete({
-  selector: '#autoCompleteCountry',
-  placeHolder: 'e.g Bali, Indonesia',
-  data: {
-    src: countryList,
-    cache: true,
-  },
-  resultItem: {
-    highlight: true,
-  },
-  events: {
-    input: {
-      selection: (event) => {
-        const selection = event.detail.selection.value;
-        autoCompleteJS.input.value = selection;
-      },
-    },
-  },
-  resultsList: {
-    maxResults: 10,
-  },
-  searchEngine: 'strict', // strict | loose
-});
+// autoCompleteFunc();
+
 // import mobileNav from './modules/mobile-nav.js';
 
 // mobileNav();
